e5beddc35e108107df3e5f933957ab7690b1126f
kubo-hide-kun <hide44041@gmail.com>
2022-03-06 03:15:47 +0900

    feat: apply linter

--- src/application.ts
+++ src/application.ts
@@ -1,38 +1,41 @@
-import express from 'express';
+import express from "express";
 
 export class Application {
-	public host: string;
-	public port: number;
-	public app: express.Express;
-	public router: express.Router;
+  public host: string;
+  public port: number;
+  public app: express.Express;
+  public router: express.Router;
 
-	constructor(host?: string, port?: number) {
-		this.host = host || 'localhost';
-		this.port = port || 3000;
-		this.app = express();
-		this.router = express.Router();
+  constructor(host?: string, port?: number) {
+    this.host = host || "localhost";
+    this.port = port || 3000;
+    this.app = express();
+    this.router = express.Router();
 
-		this.app.set('host', this.host);
-		this.app.set('port', this.port);
+    this.app.set("host", this.host);
+    this.app.set("port", this.port);
 
-		// CORSの許可
-		this.app.use((req, res, next) => {
-			res.header('Access-Control-Allow-Origin', '*');
-			res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-			next();
-		});
+    // CORSの許可
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+    });
 
-		this.setRoute();
-		this.app.use(this.router);
-	}
+    this.setRoute();
+    this.app.use(this.router);
+  }
 
-	public setRoute() {
-		throw new Error('Override method');
-	}
+  public setRoute() {
+    throw new Error("Override method");
+  }
 
-	public run() {
-		this.app.listen(this.app.get('port'), () => {
-			console.log(`Example app listening on port ${this.host}:${this.port}!`);
-		})
-	} 
+  public run() {
+    this.app.listen(this.app.get("port"), () => {
+      console.log(`Example app listening on port ${this.host}:${this.port}!`);
+    });
+  }
 }
--- src/run.ts
+++ src/run.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+
 import { Application } from './application';
 
 class Service extends Application {
-	setRoute() {
-		this.router.get('/api/getTest', (req: express.Request, res: express.Response) => {
-			res.send(req.query);
-		});
-	}
+  setRoute() {
+    this.router.get(
+      '/api/getTest',
+      (req: express.Request, res: express.Response) => {
+        res.send(req.query);
+      },
+    );
+  }
 }
 
 new Service().run();
