e0782f641b5dda3c762934a103744a024ad77ca0
kubo-hide-kun <hide44041@gmail.com>
2022-03-06 18:23:23 +0900

    feat: update responce text

--- src/Controllers/github.ts
+++ src/Controllers/github.ts
@@ -16,11 +16,25 @@ export class GithubControllerImpl {
     const { client, organizationId } = this;
 
     try {
-      const weeklyContributions =
-        await client.fetchWeeklyContributionsOfOrganizationMember(
-          organizationId,
-        );
-      res.send({ weeklyContributions });
+      const members = await client.fetchWeeklyContributionsOfOrganizationMember(
+        organizationId,
+      );
+
+      const activeMembers = members.filter(
+        (members) => !!members.contributionCount,
+      );
+      activeMembers.sort((a, b) =>
+        a.contributionCount < b.contributionCount ? 1 : -1,
+      );
+
+      const Text = activeMembers.reduce((prevText, member, idx) => {
+        let nextText = prevText;
+        nextText += `${idx}位: ${member.loginId}\n`;
+        nextText += `contribution数: ${member.contributionCount}\n\n`;
+        return nextText;
+      }, '今週のランキング:\n');
+
+      res.send(Text);
     } catch (error) {
       console.error(error);
     }
