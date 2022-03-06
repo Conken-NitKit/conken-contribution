6536c50da7ccf184272d0cddeceb56da3e2f65af
kubo-hide-kun <hide44041@gmail.com>
2022-03-06 23:29:45 +0900

    chore: create blackList.sample.ts

---
+++ src/blackList.sample.ts
@@ -0,0 +1,10 @@
+// Organizationに所属しているが今回の監視対象から除外したいユーザーを指定する。
+// export const BLACK_LIST_LOGIN_IDS = [
+//   'XXXXXX',
+//   'XXXXXX',
+//   'XXXXXX',
+//   'XXXXXX',
+//   'XXXXXX',
+//   'XXXXXX',
+// ];
+export const BLACK_LIST_LOGIN_IDS = [];
