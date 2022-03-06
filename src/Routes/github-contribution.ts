5f9f557cd664fc1b9a0c4d02dc6469d5bfca3ce7
kubo-hide-kun <hide44041@gmail.com>
2022-03-07 00:05:48 +0900

    chore: rename env variable

--- src/Routes/github-contribution.ts
+++ src/Routes/github-contribution.ts
@@ -13,7 +13,7 @@ const ORGANIZATION_ID = process.env.ORGANIZATION_ID || '';
 const HTTP_FROM_AT = Number(process.env.HTTP_FROM_AT) || 30;
 const LINE_NOTIFY_FROM_AT = Number(process.env.LINE_NOTIFY_FROM_AT) || 30;
 const MAX_SIZE = Number(process.env.MAX_SIZE) || 0;
-const MAX_MESSAGE_SIZE = Number(process.env.MAX_SIZE) || 0;
+const MAX_LINE_MESSAGE_SIZE = Number(process.env.MAX_LINE_MESSAGE_SIZE) || 0;
 
 const githubContributionController = new GithubContributionControllerImpl(
   new GitHubApiApolloClientImpl(GITHUB_ACCESS_TOKEN),
@@ -22,7 +22,7 @@ const githubContributionController = new GithubContributionControllerImpl(
   HTTP_FROM_AT,
   LINE_NOTIFY_FROM_AT,
   MAX_SIZE,
-  MAX_MESSAGE_SIZE,
+  MAX_LINE_MESSAGE_SIZE,
 );
 
 router.get(
